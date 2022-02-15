70d70327d00155b39125482660d47d283ca47c6e
Jionarran Almeida <39007823+jionarran@users.noreply.github.com>
2022-02-14 23:23:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Olá jio e larissa, testando');
+    res.send('Olá Mundo testando');
 })
 
 app.listen(port, () => {
